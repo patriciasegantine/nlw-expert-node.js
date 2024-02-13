3bfcbfca542188247265ce30bc58aaa8b1237731
Patricia Segantine <pnsegantine@gmail.com>
2024-02-13 22:42:31 +0000

    feat: poll ranking using redis

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -2,6 +2,7 @@ import { prisma } from "../../lib/prisma";
 import { z } from "zod";
 import { FastifyInstance } from "fastify";
 import { randomUUID } from "crypto";
+import { redis } from "../../lib/redis";
 
 export async function voteOnPoll(app: FastifyInstance) {
   
@@ -37,7 +38,7 @@ export async function voteOnPoll(app: FastifyInstance) {
             id: userPreviousVoteOnPoll.id
           }
         })
-        
+        await redis.zincrby(pollId, -1, userPreviousVoteOnPoll.pollOptionId)
       } else if (userPreviousVoteOnPoll) {
         return reply.status(400).send({message: 'You already voted on this poll'})
       }
@@ -61,6 +62,8 @@ export async function voteOnPoll(app: FastifyInstance) {
         pollOptionId
       }
     })
+    
+    await redis.zincrby(pollId, 1, pollOptionId)
     
     return reply.status(201).send({sessionId})
   })
