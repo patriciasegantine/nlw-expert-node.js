59c3662168825910fdf79e073e38fb7a5d78ca6d
Patricia Segantine <pnsegantine@gmail.com>
2024-02-13 14:20:58 +0000

    feat: setup project

---
+++ src/http/server.ts
@@ -0,0 +1,12 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello NLW'
+})
+
+
+app.listen({port: 3333}).then(() => {
+  console.log('HTTP server running!')
+})
