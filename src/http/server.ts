56084129718e812f554b4dcd812bda5a3e54a9d2
Patricia Segantine <pnsegantine@gmail.com>
2024-02-13 18:01:10 +0000

    feat: added prima, created migration and post polls

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,27 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello NLW'
-})
+const prisma = new PrismaClient();
 
+app.post('/polls', async (req, reply) => {
+  
+  const createPollBody = z.object({
+    title: z.string()
+  })
+  
+  const {title} = createPollBody.parse(req.body)
+  
+  const poll = await prisma.poll.create({
+    data: {
+      title,
+    }
+  })
+  
+  return reply.status(201).send({Id: poll.id})
+})
 
 app.listen({port: 3333}).then(() => {
   console.log('HTTP server running!')
