ab96bc7cd8b8391b500486abb3e21121a5ac3d4b
Patricia Segantine <pnsegantine@gmail.com>
2024-02-13 21:52:04 +0000

    feat: create migration, route and config vote

--- src/http/routes/vote-on-poll.ts
+++ src/http/routes/vote-on-poll.ts
@@ -1,3 +1,4 @@
+import { prisma } from "../../lib/prisma";
 import { z } from "zod";
 import { FastifyInstance } from "fastify";
 import { randomUUID } from "crypto";
@@ -19,6 +20,29 @@ export async function voteOnPoll(app: FastifyInstance) {
     
     let {sessionId} = req.cookies
     
+    if (sessionId) {
+      const userPreviousVoteOnPoll = await prisma.vote.findUnique({
+        where: {
+          sessionId_pollId: {
+            sessionId,
+            pollId
+          }
+        }
+      })
+      
+      if (userPreviousVoteOnPoll && userPreviousVoteOnPoll.pollOptionId !== pollOptionId) {
+        
+        await prisma.vote.delete({
+          where: {
+            id: userPreviousVoteOnPoll.id
+          }
+        })
+        
+      } else if (userPreviousVoteOnPoll) {
+        return reply.status(400).send({message: 'You already voted on this poll'})
+      }
+    }
+    
     if (!sessionId) {
       sessionId = randomUUID()
       
@@ -29,6 +53,14 @@ export async function voteOnPoll(app: FastifyInstance) {
         httpOnly: true
       })
     }
+    
+    await prisma.vote.create({
+      data: {
+        sessionId,
+        pollId,
+        pollOptionId
+      }
+    })
     
     return reply.status(201).send({sessionId})
   })
